0458d00a7ce0b85a23fcef8481b0ff2ba9d4ce61
Cuparius <5801548@FPSMA.net>
2016-09-09 13:51:14 -0400

    added cooper.py

---
+++ Cooper.py
@@ -0,0 +1,3 @@
+print ('Cooper')
+print ('I wanna be the very best, like no one ever was. To catch them is my real test, to train them is my cause. I will travel across the land, searching far and wide. Each pokemon to understand the power thats inside')
+print (Bloop)
