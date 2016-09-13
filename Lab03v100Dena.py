4427da4c1c8a4d484ef08568a1acee6f6935e344
Cuparius <5801548@FPSMA.net>
2016-09-13 09:39:06 -0400

    Lab03
    Lab03v100

---
+++ Lab03v100Dena.py
@@ -0,0 +1,26 @@
+# Lab03vST.py
+# The First Program
+# This is the Student Starting version of Lab03.
+#
+print("REQUIRED FOR 80 & 100 POINT VERSIONS") 
+print('()')
+print('****************************************')
+print('Lab 03A, THE FIRST PROGRAM')
+print('80 POINT VERSION')
+print('BY COOPER DENA')
+print('****************************************')
+print('()')
+print('A poet I am not')
+print("Don't claim to be")
+print('But what I wrote')
+print('Did come from me')
+print("")
+print("")
+print('REQUIRED FOR 100 POINT VERSION')
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10//3)
+print(10 % 3)
+print(10 ** 3)
